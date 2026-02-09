ccc58706b6872c850b6de77727d31f9f3e1e3863
wooyoung-4723 <wy4723@naver.com>
2026-02-09 15:26:39 +0900

    버튼 누르면 로또 번호가 창안에 표시되는 단계

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ lotto_num = range(1, 46)
 
 
 def buttonClick():
-    print(random.sample(lotto_num, 6))
+    label = tkinter.Label(window, text=str(random.sample(lotto_num, 6)))
+    label.pack()
 
 
 window = tkinter.Tk()
@@ -14,8 +15,8 @@ window.title("lotto")
 window.geometry("400x200")
 window.resizable(False, False)
 
-button = tkinter.Button(window, overrelief="solid",
-                        text="번호확인", width=15, command=buttonClick, repeatdelay=1000, repeatinterval=100)
+button = tkinter.Button(window, overrelief="solid", text="번호확인", width=15, command=buttonClick, repeatdelay=1000,
+                        repeatinterval=100)
 button.pack()
 
 window.mainloop()
